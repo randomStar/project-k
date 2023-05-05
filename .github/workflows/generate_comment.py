1da081ff69552708fb31527dfc8e3f94bf25c3d4
M <24471344+randomStar@users.noreply.github.com>
2023-05-05 11:51:40 +0800

    Create generate_comment.py

---
+++ .github/workflows/generate_comment.py
@@ -0,0 +1,21 @@
+import openai
+import os
+
+def generate_comment(pr_url):
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+
+    model_engine = "gpt-3.5-turbo"
+    prompt = (f"Please review the changes made in this pull request: {pr_url}. "
+              "What is good about this pull request? What needs improvement?")
+
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    comment = response.choices[0].text.strip()
+    return comment
