6abdc4bcefafd162419bdfdbb11319ba4feddc59
M <24471344+randomStar@users.noreply.github.com>
2023-05-05 11:54:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return f'Hello from pod {os.environ.get("POD_NAME")}!'
+    
+@app.route('/hello')
+def hello_again():
+    return f'Hello again from pod {os.environ.get("POD_NAME")}!'
 
 if __name__ == '__main__':
     app.run()
