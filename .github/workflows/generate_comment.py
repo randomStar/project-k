e36bba4942d86d7e7f7ef397eee853d8b9b946ea
M <24471344+randomStar@users.noreply.github.com>
2023-05-06 10:34:59 +0800

    Update generate_comment.py

--- .github/workflows/generate_comment.py
+++ .github/workflows/generate_comment.py
@@ -1,11 +1,12 @@
 import openai
 import os
-import sys
+import requests
 
 def generate_comment():
-    content = os.environ["PR_CONTENT"]
-    openai.api_key = os.environ["OPENAI_API_KEY"]
+    pr_patch = requests.get(os.environ["GITHUB_EVENT_PULL_PATCH_URL"])
+    content = pr_patch.text
 
+    openai.api_key = os.environ["OPENAI_API_KEY"]
     model_engine = "gpt-3.5-turbo"
     prompt = (f"Please act as a code reviewer and review the changes made in the following patch format pull request: {content}. ")
     messages=[{"role": "user", "content": prompt}]
