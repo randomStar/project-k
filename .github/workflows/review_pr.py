124f61833c4bccee90e15dc5b4ba1e4adf1aac74
M <24471344+randomStar@users.noreply.github.com>
2023-05-18 15:22:31 +0800

    Update review_pr.py

--- .github/workflows/review_pr.py
+++ .github/workflows/review_pr.py
@@ -4,7 +4,7 @@ import openai
 
 def call_open_ai(patch):
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    openai.endpoint = os.getenv("OPENAI_API_ENDPOINT")
+    openai.api_base = os.getenv("OPENAI_API_ENDPOINT")
     openai.api_type = os.getenv("OPENAI_API_TYPE")
     openai.api_version = os.getenv("OPENAI_API_VERSION")
 
